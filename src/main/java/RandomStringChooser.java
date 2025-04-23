d3b5102102e421d3494a128d02fbee0784766c53
brli7 <146292564+brli7@users.noreply.github.com>
2025-04-23 11:38:47 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -5,4 +5,25 @@ public class RandomStringChooser
   
   //Heads up! 
   //You will get a very confusing error message until you have working code in part b as well
+  private String[] words;
+  public RandomStringChooser(String[] strings) {
+    words = string;
+  }
+  public String getNext() {
+    boolean hasAvailable = false;
+    for(int i = 0; i < words.length; i++) {
+      if(words[i] != null) {
+        hasAvailable = true;
+      }
+    }
+    int index = (int)(Math.random()*(words.length-1));
+    if(!hasAvailable) {
+      return "NONE';
+    }
+    while(words[index] == null) {
+      index = (int)(Math.random()*(words.length-1));
+    }
+    String temp = words[index];
+    words[index] == null;
+    return temp;
 }
